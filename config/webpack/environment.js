570b71154a77a3af3a8cf663f54ba15ec508db54
Kyle Stallings <citrous@tamu.edu>
2025-02-03 16:46:24 -0600

    Update environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,4 +3,9 @@ const { environment } = require('@rails/webpacker')
 // Remove node config
 environment.config.delete('node');
 
+// Add an additional entry point
+// Explicitly define your entry points
+environment.entry.set('application', './app/javascript/application.js')
+  
+
 module.exports = environment
