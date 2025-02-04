d4cd5e220857568529f3e18bc58c3dae1095fc95
Kyle Stallings <citrous@tamu.edu>
2025-02-03 23:29:51 -0600

    Update book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-    has_many :users, through: user_books
-    has_many :user_books
+    has_many :users, through: user_book
+    has_many :user_book
     validates :title, presence: true
 end
