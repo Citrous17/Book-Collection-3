60369cce711475c4aa908b19ad5489943b6e46eb
Kyle Stallings <citrous@tamu.edu>
2025-02-03 19:10:17 -0600

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Book.create([
-    { title: "The Great Gatsby", author: "F. Scott Fitzgerald", genre: "Fiction", published_year: 1925 },
-    { title: "To Kill a Mockingbird", author: "Harper Lee", genre: "Fiction", published_year: 1960 },
-    { title: "1984", author: "George Orwell", genre: "Dystopian", published_year: 1949 },
-    { title: "Pride and Prejudice", author: "Jane Austen", genre: "Romance", published_year: 1813 },
-    { title: "The Catcher in the Rye", author: "J.D. Salinger", genre: "Fiction", published_year: 1951 },
-    { title : "The Hobbit", author: "J.R.R. Tolkien", genre: "Fantasy", published_year: 1937 },
-    { title : "Harry Potter and the Sorcerer's Stone", author: "J.K. Rowling", genre: "Fantasy", published_year: 1997 }
+Book.create!([
+    { title: "The Great Gatsby"},
+    { title: "To Kill a Mockingbird"},
+    { title: "1984", author: "George Orwell"},
+    { title: "Pride and Prejudice" },
+    { title: "The Catcher in the Rye"},
+    { title : "The Hobbit"},
+    { title : "Harry Potter and the Sorcerer's Stone"}
 ])
